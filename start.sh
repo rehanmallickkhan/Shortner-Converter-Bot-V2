2e9654d5c6b85f3980dbcd5399d09fee4f9b22a5
rehanmallickkhan <138368630+rehanmallickkhan@users.noreply.github.com>
2023-07-15 13:03:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/Cyniteofficial/Shortner-Converter-Bot-V2.git /Venom-Converter-Bot-V1
+cd /Venom-Converter-Bot-V1
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
